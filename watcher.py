85cfaa00f3b2491bcef1c6551573220b678fcda6
Dz0526 <shining.sun0526@gmail.com>
2021-01-12 23:08:03 +0000

    ver test

---
+++ watcher.py
@@ -0,0 +1,64 @@
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import subprocess
+
+import os
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# args
+parser.add_argument("target_dir", type=str, help="dir you want to watch")
+parser.add_argument("script", type=str, help="script you want to excute")
+
+args = parser.parse_args()
+
+class Watcher(FileSystemEventHandler):
+    def __init__(self, command):
+        self.command = command
+
+    def run_command(self):
+        time.sleep(0.1)
+        subprocess.run(self.command)
+
+    def on_created(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        if 'crdownload' not in filename:
+            print("%s created" %filename)
+            print("OK")
+            self.run_command()
+    """
+    def on_moved(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+    """
+
+def strTolis(string):
+    pos1 = 0
+    rtlis = []
+
+    for i, name in enumerate(string):
+        if name == " ":
+            rtlis.append(string[pos1:i])
+            pos1 = i+1
+    rtlis.append(string[pos1:i+1])
+
+    return rtlis
+
+def main():
+
+    watcher = Watcher(strTolis(args.script))
+    observer = Observer()
+    observer.schedule(watcher, args.target_dir, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+main()
